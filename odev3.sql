acfbacf31b9d02e0e8b74a0d1bf1a50887ad22d4
Melisabkr <80853579+Melisabkr@users.noreply.github.com>
2022-03-12 02:31:47 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,19 @@
+1.SORU 🔅
+
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+
+2.SORU 🔅
+
+SELECT country FROM country
+WHERE country LIKE '%_____n';
+
+3.SORU 🔅
+
+SELECT title FROM film
+WHERE title ILIKE '%T%';
+
+4.SORU 🔅
+
+SELECT * FROM film
+WHERE title LIKE 'C%'AND length > 90 AND rental_rate = 2.99;
