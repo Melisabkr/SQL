2e057b6c44d67148763b331fadf46e34f5f07d11
Melisabkr <80853579+Melisabkr@users.noreply.github.com>
2022-03-31 20:57:51 +0300

    Create odev11.sql

---
+++ odev11.sql
@@ -0,0 +1,54 @@
+1.SORU 🔅
+
+(
+SELECT first_name FROM actor
+)
+UNION
+(
+SELECT first_name FROM customer
+);
+-----
+(
+SELECT first_name FROM actor
+)
+UNION ALL
+(
+SELECT first_name FROM customer
+);
+
+
+2.SORU 🔅
+
+(
+SELECT first_name FROM actor
+)
+INTERSECT
+(
+SELECT first_name FROM customer
+);
+-----
+(
+SELECT first_name FROM actor
+)
+INTERSECT ALL
+(
+SELECT first_name FROM customer
+);
+
+3.SORU 🔅
+
+(
+SELECT first_name FROM actor
+)
+EXCEPT
+(
+SELECT first_name FROM customer
+);
+-----
+(
+SELECT first_name FROM actor
+)
+EXCEPT ALL
+(
+SELECT first_name FROM customer
+);
